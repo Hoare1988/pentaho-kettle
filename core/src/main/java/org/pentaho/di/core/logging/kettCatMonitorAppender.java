7470bdf6edfd15193dfb1fc4f565fbe518b3b72d
xuchuanhou <xuchuanhou@xforceplus.com>
2019-11-19 18:11:14 +0800

    添加依赖,错误日志可以发送到阿里云日志和监控服务器

---
+++ core/src/main/java/org/pentaho/di/core/logging/kettCatMonitorAppender.java
@@ -0,0 +1,57 @@
+package org.pentaho.di.core.logging;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.LoggingEvent;
+
+import com.xforceplus.apollo.logger.ApolloLoggerFactory;
+
+/**
+ * 将错误信息发送到监控服务器
+ * @author xuchuanhou 
+ * @date 2019年11月19日 下午5:49:06 
+ * @copyright (c) 2019, xforceplus
+ */
+public class kettCatMonitorAppender extends AppenderSkeleton {
+    
+    @Override
+    public void close() {
+       close();
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
+
+    @Override
+    protected void append(LoggingEvent event) {
+       if(event.getMessage()==null){
+            return;
+        }
+       
+        String message = event.getRenderedMessage();//JacksonUtil.getInstance().toJson(event.getRenderedMessage());
+        
+        switch (event.getLevel().toInt()) {
+            case Level.ERROR_INT:
+                ApolloLoggerFactory.getFactory().loggerError(message);
+                break;
+             case Level.WARN_INT:
+                 ApolloLoggerFactory.getFactory().loggerWarn(message);         
+                break;
+             case Level.INFO_INT:
+                 ApolloLoggerFactory.getFactory().loggerInfo(message);         
+                 break;
+             case Level.DEBUG_INT:
+                 ApolloLoggerFactory.getFactory().loggerDebug(message);         
+                 break;
+             case Level.TRACE_INT:
+                 ApolloLoggerFactory.getFactory().loggerDebug(message);         
+                 break;
+            default:
+                break;
+        }
+
+    }
+
+}
